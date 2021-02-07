325613f40579c9647fb069d7cea9145614eac06e
chea yeon <cna3762@gmail.com>
2021-02-07 13:27:37 +0900

    텍스트 파일에서 질문 불러오기 성공!!

--- app/src/main/java/com/swhackathon/polystar/InputActivity.java
+++ app/src/main/java/com/swhackathon/polystar/InputActivity.java
@@ -13,12 +13,16 @@ import android.widget.EditText;
 import android.widget.ImageButton;
 import android.widget.TextView;
 import android.widget.Toast;
+
+import java.io.ByteArrayOutputStream;
+import java.io.InputStream;
 import java.util.Random;
 
 import java.io.FileInputStream;
 import java.io.FileOutputStream;
 import java.io.IOException;
 import java.util.Calendar;
+import java.util.StringTokenizer;
 
 public class InputActivity extends AppCompatActivity {
 
@@ -26,13 +30,9 @@ public class InputActivity extends AppCompatActivity {
     EditText result;
     ImageButton checkButton;
     Calendar cal;
-    int questionCount=1;
-    String[] Strings = {"좋아하는 물건과 그 이유는 무엇인가요?", "오늘 하지 못했던 말이 있나요?",
-            "당신에게 성공이란 어떤 것인가요?", "당신에게 가족이란 어떤 의미인가요?",
-            "당신은 지금 행복한가요?","살면서 가장 잘한 일은 무엇인가요?",
-            "최근에 성취감을 느낀 것은 어떤 때였나요?","당신이 가장 하고싶은 일은 무엇인가요?",
-            "요즘 당신의 가슴을 뛰게 하는 일이 있나요?","올해 안에 가장 도전해보고 싶은 것은 무엇인가요?",
-            "오늘의 자신을 세 단어로 표현해본다면?"};
+    int questionCount=0;
+    String[] Question;
+    String questionTxt;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
@@ -43,6 +43,15 @@ public class InputActivity extends AppCompatActivity {
         result = findViewById(R.id.resultTxt);
         cal = Calendar.getInstance();
 
+        //질문 파일 생성
+        questionTxt = readTxt();
+        StringTokenizer tokens = new StringTokenizer(questionTxt);
+        Question = questionTxt.split("\n");
+
+        TextView questionTxt = findViewById(R.id.questionTxt);
+        questionTxt.setText(Question[1]);
+
+
         //답변파일 생성 변수
         int year = cal.get(Calendar.YEAR);
         int month = cal.get(Calendar.MONTH);
@@ -52,12 +61,12 @@ public class InputActivity extends AppCompatActivity {
         String s = BookmarkActivity.returnQuestion(1)+"\n"; //s대신 질문을 넣을 것!!
         String str = "abc";
 
-        while(str != null && questionCount < 4)
+        while(str != null && questionCount < 5)
         {
+            questionCount = questionCount + 1;
             filename = Integer.toString(year) + "_" + Integer.toString(month) + "_" + Integer.toString(day) + "_" + Integer.toString(questionCount);
             str = readDiary(filename);
             result.setText(str);
-            questionCount = questionCount + 1;
         }
 
 
@@ -150,9 +159,9 @@ public class InputActivity extends AppCompatActivity {
             }
         });
 
+
+
         Random random = new Random();
-
-
 
         // 질문 새로고침
         ImageButton reButton = (ImageButton) findViewById(R.id.reButton);
@@ -163,12 +172,31 @@ public class InputActivity extends AppCompatActivity {
                 TextView questionTxt = findViewById(R.id.questionTxt);   //질문란
                 EditText resultTxt = findViewById(R.id.resultTxt);   //대답란
 
-                int r = (int)(Math.random()*11);
-                questionTxt.setText(Strings[r]);
+                int r = (int)(Math.random()*12);
+                questionTxt.setText(Question[r]);
                 // 질문 새로고침 db 저장 작성
                 resultTxt.setText("");
             }
         });
+    }
+
+    private String readTxt() {
+        String data = null;
+        InputStream input = getResources().openRawResource(R.raw.question);
+        ByteArrayOutputStream bytearrayout = new ByteArrayOutputStream();
+        int i;
+        try{
+            i = input.read();
+            while (i != -1){
+                bytearrayout.write(i);
+                i = input.read();
+            }
+            data = new String(bytearrayout.toByteArray(),"MS949");
+            input.close();
+        }catch (IOException e){
+            e.printStackTrace();
+        }
+        return data;
     }
 
     //저장되어있는 답변을 읽어오는 함수
